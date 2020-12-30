b4e7c8af5b36bc92fe520b82728e5e3bd3faf787
Kristin Scott <50966226+kscott94@users.noreply.github.com>
2020-12-29 22:27:21 -0700

    Update pseudoreplicate_paired_samfile.py
    Re-worked for better efficiency

--- pseudoreplicate_paired_samfile.py
+++ pseudoreplicate_paired_samfile.py
@@ -3,80 +3,95 @@ import os
 import time
 import resource
 import argparse
+import pysam as pys
 import numpy as np
-import natsort as ns
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-i', type=str, help='path to sam file')
 parser.add_argument('-r', type=int, default = 3, help='number of desired pseudoreplicates')
+parser.add_argument('-s', type = int, default=None, help='set seed')
+parser.add_argument('-b', action = 'store_true', help='output files in bam format, sorted by coordinate')
+
 args = parser.parse_args()
 
-alignfile = args.i
-pseudo_reps = args.r
+in_file = args.i
+pseudo_replicates = args.r
 
 t1 = time.clock()
 
-# Check alignment file is sorted
-with open(alignfile, 'r') as handle:
-    handle = handle.readlines()[-100:-1]
-    qnames = []
-    for line in handle:
-        qname = line.partition("\t")[0]
-        qnames.append(qname)
-    if qnames != ns.natsorted(qnames):
-        raise Exception("Please sort your alignment file by name. You can do this using samtools sort -n <file.sam> -o <sorted_file.sam>")
+in_file_name, in_file_ext = os.path.splitext(in_file)
 
-header = []
-alignment_ids_init = set()
-print('Your alignment file is being split')
+# Check file extension
+if not args.b and in_file_ext == ".bam":
+    print('Your file must have a .sam extension.')
+    print('If you are using a bam file, use -b flag.')
+    exit(0)
+
+# Sort alignment file by name
+alignfile = str(in_file_name) + "_nsorted_tmp.sam"
+pys.sort("-o", alignfile, "-n", in_file)
+
+print('Getting ready...')
 
 # Open samfile, separate header lines, and create a set of qnames
-with open(alignfile, 'r') as samfile:
-    for line in samfile:
+header = []
+read_count = 0
+
+with open(alignfile, 'r') as f:
+    for line in f.readlines():
         if line[0] == "@":
             header.append(line)
         else:
-            c1 = line.partition('\t')[0]
-            alignment_ids_init.add(c1)
+            read_count +=1
 
-alignment_ids = ns.natsorted(alignment_ids_init)
+print('Your alignment file is being split...')
 
 # Create a new file for each replicate, and add the header
 split_file_list = []
-samfile_name, samfile_ext = os.path.splitext(alignfile)
 
-for i in range(pseudo_reps):
+for i in range(pseudo_replicates):
     i += 1
-    name = samfile_name + "_split" + str(i) + samfile_ext
+    if args.b:
+        name = in_file_name + "_split" + str(i) + ".bam"
+    else:
+        name = in_file_name + "_split" + str(i) + ".sam"
     with open(name, "w") as samfile_split:
         split_file_list.append(name)
         for j in range(len(header)):
             samfile_split.write(header[j])
 
-# generate a random integer for each unique alignment id (qname). This integer will ensure the alignemnt id goes to the correspoding pseudoreplicate file
-alignment_outbound_id = []
-alignment_outbound_int = []
-
-#np.random.seed(args.s)   # doesn't work
-for id in alignment_ids:
-    pseudo_rep = np.random.randint(1,pseudo_reps+1)
-    alignment_outbound_id.append(id)
-    alignment_outbound_int.append(pseudo_rep)
-
-alignment_outbound = list(zip(alignment_outbound_id, alignment_outbound_int))
-
 # Add alignments to new file
-with open(alignfile, 'r') as samfile:
-    samfile = samfile.readlines()[len(header):]
-    for row in samfile:
-        r1 = row.partition("\t")[0]
-        current_index = 0
-        for id, rep in alignment_outbound[current_index:]:
-            if id == r1:
-                current_index = alignment_outbound.index((id,rep))
-                with open(split_file_list[rep-1], 'a') as split_file_pseudorep:
-                    split_file_pseudorep.write(row)
-                break
+with open(alignfile, 'r') as f:
+    samfile = f.readlines()[len(header):]     # start reading file at first read
+
+    # Write the first read into a split file
+    with open(split_file_list[0], 'a') as f1:
+        f1.write(samfile[0])
+
+    # Initialize parameters for the rest of the reads
+    index = 1  # The first read is at index 0. Starting at
+    int = 0    # The first file containing reads is called _split1.sam
+    # Begin partitioning reads into files randomly, keeping pairs together
+    np.random.seed(args.s)  # doesn't work
+    for i in range(read_count-1):
+        if samfile[index].partition("\t")[0] == samfile[index-1].partition("\t")[0]:
+            with open(split_file_list[int], 'a') as f2:
+                f2.write(samfile[index])
+        else:
+            int = np.random.randint(pseudo_replicates)
+            with open(split_file_list[int], 'a') as f3:
+                f3.write(samfile[index])
+        index += 1
+
+#convert output to bam is args.b, and remove all temp files
+
+if args.b:
+    for file in split_file_list:
+        name, ext = os.path.splitext(file)
+        split_out_name = str(name) + '.bam'
+        pys.sort("-o", split_out_name, file)
+
+os.remove(alignfile)
 
 t2 = time.clock()
 mem = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
